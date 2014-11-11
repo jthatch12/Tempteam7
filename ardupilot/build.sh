89a5d4bb5228b1a83604a75c1761bcf73f7efefe
John Thatcher <john_thatcher@ymail.com>
2014-11-11 12:56:09 -0500

    Initial commit

---
+++ ardupilot/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /ArduCopter
+
+make apm2
---
+++ hcsr04/hcsr04.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <prussdrv.h>
+#include <pruss_intc_mapping.h>
+
+int main(void) {
+
+	/* Initialize the PRU */
+	printf(">> Initializing PRU\n");
+	tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+	prussdrv_init();
+
+	/* Open PRU Interrupt */
+	if (prussdrv_open (PRU_EVTOUT_0)) {
+		// Handle failure
+		fprintf(stderr, ">> PRU open failed\n");
+		return 1;
+	}
+
+	/* Get the interrupt initialized */
+	prussdrv_pruintc_init(&pruss_intc_initdata);
+
+	/* Get pointers to PRU local memory */
+	void *pruDataMem;
+	prussdrv_map_prumem(PRUSS0_PRU0_DATARAM, &pruDataMem);
+	unsigned int *pruData = (unsigned int *) pruDataMem;
+
+	/* Execute code on PRU */
+	printf(">> Executing HCSR-04 code\n");
+	prussdrv_exec_program(0, "hcsr04.bin");
+
+	/* Get measurements */
+	unsigned int last = 0;
+	unsigned int error_value = 24350; 
+	int i=1;
+	while (i++ != 0) {
+		
+		// Wait for the PRU interrupt
+		prussdrv_pru_wait_event (PRU_EVTOUT_0);
+		prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+		
+		// Print the distance received from the sonar
+		// At 20 degrees in dry air the speed of sound is 342.2 cm/sec
+		// so it takes 29.12 us to make 1 cm, i.e. 58.44 us for a roundtrip of 1 cm
+		
+		//Use last valid value if it errors out, otherwise, save the current value for future use
+		if(pruData[0] == error_value){
+			pruData[0] = last;
+		} else {
+			last = pruData[0];
+		}
+		
+		printf("%3d: Distance = %.2f cm\n", i, (float) pruData[0] / 58.44);
+		sleep(0);
+	}
+
+	/* Disable PRU and close memory mapping*/
+	prussdrv_pru_disable(0);
+	prussdrv_exit();
+	printf(">> PRU Disabled.\r\n");
+	
+	return (0);
+
+}
